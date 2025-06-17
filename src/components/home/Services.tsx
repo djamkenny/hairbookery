c12fa9b37620c99a9bffb705295134faaa30596c
Enoch Kenny Djam Beamahn (Kenny_BED) <113642671+djamkenny@users.noreply.github.com>
2025-06-17 00:17:48 +0000

    update

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -56,7 +56,7 @@ const services = [
     description: "Quality hair extensions applied by certified professionals for added length and volume.",
     price: "GHS40+",
     duration: "180 mins",
-    image: "./images/extension.jpg",
+    image: "./images/extension.png",
     category: "hair"
   },
   {
